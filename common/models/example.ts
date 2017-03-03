e9e349ecc1507314cf24f3498bc49d1c078712a5
Dementyev Alexey <adementyev@zaoblako.ru>
2017-03-03 18:22:13 +0300

    add other operation hooks

--- common/models/example.ts
+++ common/models/example.ts
@@ -5,7 +5,12 @@ import {Model} from "@mean-expert/model";
  **/
 @Model({
   hooks: {
+    access: {name: 'access', type: 'operation'},
+    persist: {name: 'persist', type: 'operation'},
+    afterSave: {name: 'after save', type: 'operation'},
     beforeSave: {name: 'before save', type: 'operation'},
+    beforeDelete: {name: 'before delete', type: 'operation'},
+    afterDelete: {name: 'after delete', type: 'operation'},
     beforeMyRemote: {name: 'myRemote', type: 'beforeRemote'},
     afterMyRemote: {name: 'myRemote', type: 'afterRemote'},
   },
@@ -19,6 +24,16 @@ import {Model} from "@mean-expert/model";
 
 class account {
   constructor(public model: any) {
+  }
+
+  access(ctx: any, next: Function): void {
+    console.log('example: access');
+    next();
+  }
+
+  persist(ctx: any, next: Function): void {
+    console.log('example: persist');
+    next();
   }
 
   beforeSave(ctx: any, next: Function): void {
@@ -38,6 +53,16 @@ class account {
 
   afterMyRemote(ctx: any, next: Function) {
     console.log('example: after myRemote');
+    next();
+  }
+
+  beforeDelete(ctx: any, next: Function): void {
+    console.log('example: before Delete');
+    next();
+  }
+
+  afterDelete(ctx: any, next: Function): void {
+    console.log('example: after Delete');
     next();
   }
 
